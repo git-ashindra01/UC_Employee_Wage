d34a62c37d4a370b9bfd175befb967ff5956ff4e
Pragyan <245512@NTTDATA.COM>
2022-12-27 05:27:56 +0530

    ADDED: Code for Employee Attendance Check

--- UCEmployeeWage/src/com/bridgelabz/employeewage/EmployeeWage.java
+++ UCEmployeeWage/src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,8 +1,27 @@
 package com.bridgelabz.employeewage;
-import java.util.Scanner;
+
+import java.util.Random;
 
 public class EmployeeWage {
+	
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		Random random = new Random();
+		
+		Boolean empCheck = random.nextBoolean();  
+		// We could have achieved the same thing with the below code
+		// double empCheck = Math.floor(Math.random() * 10) % 2;
+		
+		if (empCheck == true) {
+	
+			System.out.println("Employee is Present");
+	
+		} else {
+			
+			System.out.println("Employee is Absent");
+		
+		}
+	
 	}
+
 }
