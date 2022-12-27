f8a301820f71f4343947fe72b02272a796842504
Pragyan <245512@NTTDATA.COM>
2022-12-27 06:05:34 +0530

    MODIFIED : Code added for Emp. Part Time

--- UCEmployeeWage/src/com/bridgelabz/employeewage/EmployeeWage.java
+++ UCEmployeeWage/src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -8,12 +8,15 @@ public class EmployeeWage {
 		
 		int EMP_WAGE_HOUR = 20;
 		int FULL_DAY_HOUR = 8;
+		int PART_DAY_HOUR = 4;
 		
 		int emp_Daily_Wage = EMP_WAGE_HOUR * FULL_DAY_HOUR;
+		int emp_PartTime_Wage = EMP_WAGE_HOUR * PART_DAY_HOUR;
+		
 		
 		System.out.println("Employee Daily Wage for 8 Hours in a Day is : " + emp_Daily_Wage);
+		System.out.println("Employee Part Time Wage for 4 Hours in a Day is : " + emp_PartTime_Wage);
 		
-	
 	}
 
 }
