0a3dc5708c9a8fb68e9cfb25cd5f80e96a6af91f
Pragyan <245512@NTTDATA.COM>
2022-12-28 05:45:49 +0530

    MODIFIED: Code for Emp. Wages Full/Part. Using Switch

--- UCEmployeeWage/src/com/bridgelabz/employeewage/EmployeeWage.java
+++ UCEmployeeWage/src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -9,31 +9,32 @@ public class EmployeeWage {
 		Random random = new Random();
 		
 		int empCheck = random.nextInt(3) + 1;
-			
-		if (empCheck > 1) {
-			
-			System.out.println("Employee is present");
-			
-			int EMP_WAGE_HOUR = 20;
-			
-			if (empCheck == 2) {
-				int FULL_DAY_HOUR = 8;
-				int emp_Daily_Wage = EMP_WAGE_HOUR * FULL_DAY_HOUR;
-				System.out.println("Employee Daily Wage for 8 Hours in a Day is : " + emp_Daily_Wage);
-				
-			} else {
-				int PART_DAY_HOUR = 4;
-				int emp_PartTime_Wage = EMP_WAGE_HOUR * PART_DAY_HOUR;
-				System.out.println("Employee Part Time Wage for 4 Hours in a Day is : " + emp_PartTime_Wage);
-				
-			}
-			
-		} else {
-			
-			System.out.println("Employee is absent, his Wage is Zero");
-			
-		}	
 		
+		int EMP_WAGE_HOUR = 20;
+		
+		switch (empCheck) {
+		
+			case 1:
+				int FULL_DAY_HOUR = 8;
+				int emp_Daily_Wage = empWage ( EMP_WAGE_HOUR , FULL_DAY_HOUR);
+				System.out.println("Employee Daily Wage for 8 Hours in a Day is : " + emp_Daily_Wage);
+				break;
+		
+			case 2:
+				int PART_DAY_HOUR = 4;
+				int emp_PartTime_Wage = empWage ( EMP_WAGE_HOUR , PART_DAY_HOUR );
+				System.out.println("Employee Part Time Wage for 4 Hours in a Day is : " + emp_PartTime_Wage);
+				break;
+			
+			default:
+				System.out.println("Employee is Absent, Wage is Zero");
+		}		
 	}
-
+		
+	
+	public static int empWage ( int x , int y ) {
+		
+		return x * y;
+	}
+	
 }
